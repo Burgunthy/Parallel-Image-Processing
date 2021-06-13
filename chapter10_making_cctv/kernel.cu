06c94440945730619ac7dfe3b7ce06df3210596e
Burgunthy <newtree8@naver.com>
2021-06-13 15:51:24 +0900

    Gaussian Downsampling

--- chapter10_making_cctv/kernel.cu
+++ chapter10_making_cctv/kernel.cu
@@ -3,4 +3,58 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
-extern "C" void CUDA_resize(uchar* frame);
+extern "C" void CUDA_Gaussian_Filter(uchar *pcuSrc, uchar *pcuDst, int w, int h, float *cuGkernel, int kernel_size);
+
+__constant__ float constKernel[5 * 5];
+
+__global__
+void cuda_constant_Filter_2D(uchar * pSrcImage, uchar *pDstImage,
+	int SrcWidth, int SrcHeight, int KWidth, int KHeight)
+{
+	// 블록과 쓰레드 주소에 따라 현재 pixel의 index를 계산한다
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
+	int tz = threadIdx.z;
+
+	int x = blockIdx.x*blockDim.x + tx;
+	int y = blockIdx.y*blockDim.y + ty;
+
+	int index = y * SrcWidth * 3 + x * 3 + tz;
+	int mSize = KWidth / 2;
+
+	float temp = 0.f;
+
+	// Serial과 동일하게 예외처리 진행 후 각 pixel 계산
+	if (x >= KWidth / 2 && y >= KHeight / 2
+		&& x < SrcWidth - KWidth / 2 && y < SrcHeight - KHeight / 2)
+	{
+		for (int j = -mSize; j <= mSize; j++) {
+			for (int i = -mSize; i <= mSize; i++) {
+
+				// float 형태로 계산 및 저장
+				temp += (float)pSrcImage[index + i * 3 + j * SrcWidth * 3]
+					* constKernel[i + mSize + (j + mSize) * KHeight];
+			}
+		}
+		// 최종 dst 이미지에는 uchar의 형태로 저장
+		pDstImage[index] = (uchar)temp;
+	}
+	else {
+		pDstImage[index] = 0;		// kernel size 바깥의 픽셀의 예외처리 진행
+	}
+}
+
+void CUDA_Gaussian_Filter(uchar *pcuSrc, uchar *pcuDst,
+	int w, int h, float *cuGkernel, int kernel_size) {
+	// 16 x 16 쓰레드의 블록과 그 grid 사이즈 저장
+	dim3 grid = dim3(w / 16, h / 16, 1);
+	dim3 block = dim3(16, 16, 3);
+
+	// 각 pixel 별 CUDA Gaussain Filter 진행
+
+	cudaMemcpyToSymbol(constKernel, cuGkernel, sizeof(float)*kernel_size*kernel_size);
+	cuda_constant_Filter_2D << < grid, block >> > (pcuSrc, pcuDst, w, h, kernel_size, kernel_size);
+
+	// 메모리 싱크로나이즈 진행
+	cudaThreadSynchronize();
+}
--- chapter10_making_cctv/main.cpp
+++ chapter10_making_cctv/main.cpp
@@ -1,9 +1,13 @@
+
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
 
 #include <omp.h>
 #include <ipp.h>
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
@@ -19,64 +23,183 @@ using namespace cv;
 #define localWidth (resultWidth / (sqrt(num)))
 #define localHeight (resultHeight / (sqrt(num)))
 
-extern "C" void CUDA_resize(uchar* frame);
+float fpssum = 0.f;
+int iteration = 0;
+
+// cu파일과의 연동을 위해 main code에 extern "C"로 함수 선언
+extern "C" void CUDA_Gaussian_Filter(uchar *pcuSrc, uchar *pcuDst, int w, int h, float *cuGkernel, int kernel_size);
 
 // 비디오 불러오기
-void getFrame(Mat *frame, VideoCapture *cap) {
+bool getFrame(Mat *frame, VideoCapture *cap) {
 #pragma omp parallel for
 	for (int i = 0; i < num; i++) {
 		cap[i] >> frame[i];
-		//imshow(to_string(i), frame[i]);
+		if (frame[i].empty()) {
+			cout << "mosiac finish!!\n";
+			cout << "avg fps : " << fpssum / iteration << endl;
+		}
 	}
 }
 
-void ocvResize(Mat *frame, int width, int height) {
+void ippGaussianBlur(Mat &src, Mat&dst, int w, int h, int kernel_size, int sigma) {
 
-	for (int i = 0; i < num; i++) {
-		resize(frame[i], frame[i], Size(width, height));
+	IppiSize tsize;
+	// get the size of ROI
+	tsize.width = w * 3;
+	tsize.height = h - 3;
+	int srcStep = 0, dstStep = 0;
+
+	Ipp8u *S_img = (Ipp8u *)ippiMalloc_8u_C3(tsize.width, tsize.height, &srcStep);	// src IPP data
+	Ipp8u *T = (Ipp8u *)ippiMalloc_8u_C3(tsize.width, tsize.height, &srcStep);		// dst IPP data
+
+	ippiCopy_8u_C3R((const Ipp8u*)src.data, tsize.width, S_img, tsize.width, tsize);
+
+	int iTmpBufSize = 0, iSpecSize = 0;		// Buffer size, iSpec size
+	ippiFilterGaussianGetBufferSize(		// get buffer, iSpec size in kernel and ROI size
+		tsize,					// ROI size
+		kernel_size,			// kernel size
+		ipp8u,					// IPP data type
+		3,						// number of channel
+		&iSpecSize, &iTmpBufSize);
+
+	IppFilterGaussianSpec* pSpec = (IppFilterGaussianSpec *)ippsMalloc_8u(iSpecSize);
+	Ipp8u* pBuffer = ippsMalloc_8u(iTmpBufSize);
+	Ipp8u* border = new Ipp8u[3];
+	border[0] = 255;
+	border[1] = 255;
+	border[2] = 255;
+
+	// start Gaussian Blur and get blurred IPP data T
+	ippiFilterGaussianInit(tsize, kernel_size, sigma, ippBorderConst, ipp8u, 3, pSpec, pBuffer);
+	ippiFilterGaussianBorder_8u_C3R(S_img, tsize.width, T, tsize.width, tsize, border, pSpec, pBuffer);
+
+	ippiCopy_8u_C3R(T, tsize.width, (Ipp8u*)src.data, tsize.width, tsize);
+
+	// release
+	ippiFree(pBuffer);
+	ippiFree(pSpec);
+	ippiFree(S_img);
+	ippiFree(T);
+}
+
+void ompDownsampling(Mat &src, Mat&dst, int width, int height) {
+	int hindex;
+	int windex;
+
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+
+			hindex = j * src.rows / height;
+			windex = i * src.cols / width;
+
+			for (int k = 0; k < 3; k++) {
+				dst.at<Vec3b>(j, i)[k] = src.at<Vec3b>(hindex, windex)[k];
+			}
+		}
 	}
 }
 
-void ompResize(Mat *frame, int width, int height) {
+void ippGaussianDownsampling(Mat &src, Mat&dst, int ksize, int width, int height) {
 
+	ippGaussianBlur(src, src, src.cols, src.rows, ksize, 3);
+	ompDownsampling(src, dst, width, height);
 }
 
-void ippResize(Mat *frame, int width, int height) {
+void Gaussian_Kernel_2D(int kernel_size, float sigma, float *kernel) {
+	int mSize = kernel_size / 2;
+	float sum = 0.0f;
 
+	// kernel_size * kernel_size에 알맞은 값을 저장한다
+	for (int y = -mSize; y <= mSize; y++) {
+		for (int x = -mSize; x <= mSize; x++) {
+			kernel[x + mSize + (y + mSize) * kernel_size] =
+				exp(-(pow(x, 2) + pow(y, 2)) / (2 * sigma * sigma)) /
+				(2 * (float)CV_PI * sigma * sigma);
+			sum += kernel[x + mSize + (y + mSize) * kernel_size];
+		}
+	}
+
+	// kernel 총 덧셈 시 1이 되도록 normalization 진행
+	for (int i = 0; i < kernel_size * kernel_size; i++) {
+		kernel[i] /= sum;
+	}
 }
 
-void sseResize(Mat *frame, int width, int height) {
+void cudaGaussian(Mat &src, Mat &dst, int w, int h, float *kernel, int kernel_size) {
+	uchar *pcuSrc;
+	uchar *pcuDst;
+	float *pcuGkernel;
 
+	// 처음을 바꾸면 엄청 빨라지네?
+	/*(cudaMalloc((void**)&pcuSrc, w * h * 3 * sizeof(uchar)));
+	(cudaMalloc((void**)&pcuDst, w * h * 3 * sizeof(uchar)));*/
+
+	(cudaMalloc((void**)&pcuSrc, 1 * sizeof(uchar)));
+	(cudaMalloc((void**)&pcuDst, 1 * sizeof(uchar)));
+
+	(cudaMalloc((void**)&pcuGkernel, kernel_size * kernel_size * sizeof(float)));
+
+	// kernel과 src 데이터 복사
+	(cudaMemcpy(pcuSrc, src.data, w*h * 3 * sizeof(uchar), cudaMemcpyHostToDevice));
+	(cudaMemcpy(pcuGkernel, kernel, kernel_size * kernel_size * sizeof(float), cudaMemcpyHostToDevice));
+
+	// 쿠다 GPU Filter code
+	CUDA_Gaussian_Filter(pcuSrc, pcuDst, w, h, pcuGkernel, kernel_size);
+
+	// 출력 영상에 GPU data를 저장
+	(cudaMemcpy(dst.data, pcuDst, w * h * 3 * sizeof(uchar), cudaMemcpyDeviceToHost));
+
+	// free the device memory
+	cudaFree(pcuSrc);
+	cudaFree(pcuDst);
+	cudaFree(pcuGkernel);
 }
 
-void cudaResize(Mat *frame, int width, int height) {
+void cudaGaussianDownsampling(Mat &src, Mat&dst, int width, int height, int ksize, float *kernel) {
 
+	cudaGaussian(src, src, src.cols, src.rows, kernel, ksize);
+	ompDownsampling(src, dst, width, height);
 }
 
 void catImage(Mat &src, Mat &result, int i, int j) {
+	// frame의 모든 data를 result의 정확한 위치에 복사
 	for (int y = 0; y < localHeight; y++) {
 		for (int x = 0; x < localWidth; x++) {
 
 			for (int k = 0; k < 3; k++) {
-				result.at<Vec3b>(j * localHeight + y, i * localWidth + x)[k] = src.at<Vec3b>(y, x)[k];
+				// result의 특정 위치에 현재 data 복사 
+				result.at<Vec3b>(j * localHeight + y, i * localWidth + x)[k]
+					= src.at<Vec3b>(y, x)[k];
 			}
 
 		}
 	}
 }
 
-void makeCCTV(Mat *frame, Mat result) {
-	// 하나의 이미지로 변경한다
+void makeMosaic(Mat *frame, Mat result) {
+	// 모든 frame을 하나의 result로 병합한다
 
 #pragma omp parallel for
 	for (int i = 0; i < num; i++) {
+	// 4 x 4의 정확한 위치에 frame 복사
 		catImage(frame[i], result, i % (int)sqrt(num), i / (int)sqrt(num));
+	}
+}
+
+void Downsampling(Mat *frame, int *ksize, int maxksize, float *kernel, int width, int height) {
+#pragma omp parallel for
+	for (int i = 0; i < num; i++) {
+		ippGaussianDownsampling(frame[i], frame[i], ksize[i], width, height);
+		//cudaGaussianDownsampling(frame[i], frame[i], width, height, maxksize, kernel);
 	}
 }
 
 int main() {
 
-	// 비디오 불러오기 -> 16개 openmp 사용
+	int kernel_size = 5;
+	int sigma = 3;
+	float *Gkernel = new float[kernel_size * kernel_size];
+	Gaussian_Kernel_2D(kernel_size, sigma, Gkernel);
 
 	string *strVideo = new string[num];
 	VideoCapture *cap = new VideoCapture[num];
@@ -89,31 +212,40 @@ int main() {
 		cap[i] = VideoCapture(strVideo[i]);
 	}
 
+	getFrame(frame, cap);
+	int krate;
+	int *ksize = new int[num];
+	for (int i = 0; i < num; i++) {
+		krate = frame[i].cols / localWidth;
+		ksize[i] = (krate % 2 == 0) ? krate + 1 : krate;
+	}
+
 	double fstart, fend, fprocTime;
 	double fps;
 
 	while (1) {
 		fstart = omp_get_wtime();
 
-		// 비디오 불러오기
+		// get videos using OpenMP
 		getFrame(frame, cap);
 
-		// 모든 비디오 resize
-		ocvResize(frame, localWidth, localHeight);
+		// downsampling videos
+		Downsampling(frame, ksize, kernel_size, Gkernel, localWidth, localHeight);
 
-		
-
-		// 모든 비디오 하나의 Mat으로 합침
-		makeCCTV(frame, result);
+		// Merge all videos
+		makeMosaic(frame, result);
 
 		// fps 체크
 		fend = omp_get_wtime();
 		fprocTime = fend - fstart;
 		fps = (1 / (fprocTime));
 		// check fps and show text
-		putText(result, "fps : " + to_string(fps), Point(100, 100), FONT_HERSHEY_SIMPLEX, 2, Scalar(255, 0, 0), 3);
+		putText(result, "fps : " + to_string(fps),
+			Point(100, 100), FONT_HERSHEY_SIMPLEX, 2, Scalar(255, 0, 0), 3);
 
-		// 출력
+		cout << "interation " << iteration << " fps : " << fps << "\n";
+		iteration++;	fpssum += fps;
+
 		imshow("result", result);
 		if (waitKey(27) == 27) break;
 	}
